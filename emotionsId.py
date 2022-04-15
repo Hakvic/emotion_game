8ac2d4659e1da9083027aeda36c866d748923174
Eva Cn <cheneva@outlook.fr>
2022-04-15 12:49:39 +0200

    image recognition update

---
+++ emotionsId.py
@@ -0,0 +1,24 @@
+import enum
+from imaplib import Time2Internaldate
+from typing import SupportsIndex
+# Using enum class create enumerations
+class Emotions(enum.Enum):
+   Drôle = 10
+   Exité = 11
+   Heureux = 12
+   Calme = 13
+   Timide = 14
+   Incdecis = 15 
+   Fatigué = 16
+   Surpris = 17
+   Deçu = 18
+   Anxieux =19
+   Contrarié =20
+   Boudeur = 21
+   Méprisant =22
+   Entêté = 23
+   Fâché = 24
+   Frustré = 25
+   Apeuré = 26
+   Triste = 27
+   Mélancolique = 28 
--- qt_image_recognition.py
+++ qt_image_recognition.py
@@ -1,30 +1,60 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from select import select
+from xml.etree.ElementTree import tostring
+from emotionsId import Emotions
 
 # import sys
 import rospy
 import cv2
 import threading
+import random
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from qt_nuitrack_app.msg import Faces, FaceInfo
 from std_msgs.msg import Float32MultiArray
 
 
 class image_converter:
-    faces = None
-    faces_time = None
-
+   
     def __init__(self):
         self.lock = threading.Lock()
         self.bridge = CvBridge()
         self.image_pub = rospy.Publisher("/face_recognition/out", Image, queue_size=1)
         self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
         self.object_sub= rospy.Subscriber('/find_object/objects', Float32MultiArray, self.image_callback)
-
+        self.speechSay_pub = rospy.Publisher('/qt_robot/speech/say', String, queue_size=10)
+        self.randomEmotionId=0
+      
+    def startGame(self):
+        self.randomEmotionId = self.selectRandomEmotion()
+        self.speak("Montre moi l'expression suivante : " + Emotions(self.randomEmotionId))          
+   
+    def speak(self, message_a_dire):
+        # On attend la connection
+        date_debut = rospy.get_time()
+        while (self.speechSay_pub.get_num_connections() == 0) :
+            rospy.loginfo("en attente d'une réponse du publisher")
+            if rospy.get_time() - date_debut > 5.0:
+                rospy.logerr("La connexion avec le publisher n'a pas pu être établie ! Abandon...")
+                sys.exit()
+            rospy.sleep(1)
+        print(message_a_dire)
+        self.speechSay_pub.publish(message_a_dire) # On envoie au robot la phrase à dire
+    
+    
     def image_callback(self,msg):
-        print(msg.data)
+        if(self.randomEmotionId!=0 & msg.data.tostring != '()'):
+            print(msg.data)
+        
+    def selectRandomEmotion(self):
+        return randrange(10, 28)
+         
+    def match_image(self,emotionToFind):
+       return Emotions[emotionToFind]
+   
+            
+        
 
     def callback(self,data):
         try:
@@ -35,11 +65,13 @@ class image_converter:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         except CvBridgeError as e:
             print(e)
+            
 
 
 if __name__ == '__main__':
     rospy.init_node('object_recognition', anonymous=True)
     ic = image_converter()
+    ic.startGame()
     try:
         rospy.spin()
     except KeyboardInterrupt:
